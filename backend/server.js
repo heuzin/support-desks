8f078155765b09a0649b73c412339f3bb5575260
Matheus Silva <msilvaqs@outlook.com>
2026-04-10 10:37:46 -0400

    Connect to the database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv");
 const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 
 dotenv.config();
+// Connect to MongoDB
+connectDB();
+
 const PORT = process.env.PORT || 8000;
 
 const app = express();
